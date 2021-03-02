2aca1f5e397f9d02dc43773e7218358b04a7d9b2
sjors hoek <sjors.hoek@hotmail.com>
2021-03-02 12:04:48 +0100

    new, create, update, edit, delete, buttons in views and create new + edit views

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :problem, presence: true
+  validates :solution, presence: true
+  validates :address, presence: true
 end
