c04a04547ec39d3259e40e5f3661aa2219cf9f68
monica andreoli <monicandreoli@gmail.com>
2021-03-02 12:36:35 +0100

    cloudinary added

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
+
   validates :title, presence: true
   validates :problem, presence: true
   validates :solution, presence: true
