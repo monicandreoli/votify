1d15522dab1b95f9ac1f626d693dd24ddbc5b94b
monica andreoli <monicandreoli@gmail.com>
2021-03-04 11:41:02 +0100

    ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,9 @@
 class VotesController < ApplicationController
     def create
       @idea = Idea.find(params[:idea_id])
-      @idea.votes.create(user_id: current_user.id)
-      redirect_to idea_path(@idea)
+      @vote = @idea.votes.create(user_id: current_user.id)
+      @vote.save
+      redirect_to idea_path(@idea, anchor: "vote-#{@vote.id}")
     end
 
     def destroy
