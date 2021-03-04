c692d544866d6c906242a4d9241c5547f3fed955
monica andreoli <monicandreoli@gmail.com>
2021-03-04 12:47:02 +0100

    ajax working and method in model idea

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,16 @@
 class VotesController < ApplicationController
     def create
       @idea = Idea.find(params[:idea_id])
-      @vote = @idea.votes.create(user_id: current_user.id)
+      @idea.votes.create(user_id: current_user.id)
+      @vote = @idea.pre_vote_find(current_user)
       @vote.save
-      redirect_to idea_path(@idea, anchor: "vote-#{@vote.id}")
+      redirect_to idea_path(@idea)
     end
 
     def destroy
       @vote = Vote.find(params[:id])
       @vote.destroy
+      # redirect_to idea_path(@vote.idea)
       redirect_to idea_path(@vote.idea)
     end
 end
