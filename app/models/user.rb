54ffdee1f6652628e467a6262fde888ad5d21ed2
monica andreoli <monicandreoli@gmail.com>
2021-03-08 14:42:42 +0100

    dashboard municipality

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :ideas
   has_many :votes
   has_many :voted_ideas, through: :votes, class_name: 'Idea', foreign_key: :idea_id, source: :idea
+  validates :role, inclusion: { in: %w(Resident Municipality) }
   has_one_attached :photo
 
 end
