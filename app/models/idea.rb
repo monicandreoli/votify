e93f4fb6f91f720d280d08f208bb894d1ce8fc91
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-03-09 10:59:51 +0100

    Create seeds for resident&municipality users

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -8,8 +8,6 @@ class Idea < ApplicationRecord
   validates :solution, presence: true
   validates :address, presence: true
   validate :allow_vote
-  # validates :municipality, presence: true
-  # validates :participators, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
