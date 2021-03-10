ab0c385b4dae366554b9e929bd83d30bb0867eda
Joanna Lisiak <joanna.lisiak13@gmail.com>
2021-03-10 17:01:24 +0100

    Change default idea status to nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_113223) do
+ActiveRecord::Schema.define(version: 2021_03_10_154007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_113223) do
     t.string "address"
     t.integer "goal", default: 12
     t.date "deadline"
-    t.string "status", default: "Pending"
+    t.string "status"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
