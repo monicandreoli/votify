5f6662e333bfacd1e3dd93871d7562f1eb5ac38c
monica andreoli <monicandreoli@gmail.com>
2021-03-11 18:31:44 +0100

    ajax chat

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,16 +1,18 @@
 import consumer from "./consumer";
 
+
 const initChatroomCable = () => {
   const messagesContainer = document.getElementById('messages');
+  const contentForm = document.getElementById('message_content');
   if (messagesContainer) {
     const id = messagesContainer.dataset.chatroomId;
-
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-        received(data) {
-          messagesContainer.insertAdjacentHTML('afterbegin', data);
-        }
+      received(data) {
+        messagesContainer.insertAdjacentHTML('afterbegin', data);
+        contentForm.value = "";
+      }
       });
+    }
   }
-}
 
 export { initChatroomCable };
