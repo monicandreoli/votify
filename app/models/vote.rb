acd02bf137bcd10a96361513d6205ead3be260e2
monica andreoli <monicandreoli@gmail.com>
2021-03-12 10:02:01 +0100

    style

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,9 @@ class Vote < ApplicationRecord
   belongs_to :idea
   belongs_to :user
 
+  # with this validation the vote is unique for every user
+  validates :user, uniqueness: { scope: :idea }
+
   after_create :check_idea_status
 
   def check_idea_status
